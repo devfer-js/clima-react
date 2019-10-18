bccf3ac36ff74feb15dd68e62ad7747fb9d8cfe9
ferkki <ferkki@hotmail.com>
2019-10-18 18:24:00 -0400

    Updates

---
+++ precache-manifest.9293b5dd81ca065f7ebae644af76d617.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dcae58cf707c4d0ed1f88f29c22646",
+    "url": "/clima-react/index.html"
+  },
+  {
+    "revision": "bef25d47f638a0674e74",
+    "url": "/clima-react/static/css/main.8674dd7f.chunk.css"
+  },
+  {
+    "revision": "e358e4d0acd6efd647e2",
+    "url": "/clima-react/static/js/2.15ab4c7a.chunk.js"
+  },
+  {
+    "revision": "bef25d47f638a0674e74",
+    "url": "/clima-react/static/js/main.f3b4a384.chunk.js"
+  },
+  {
+    "revision": "d94f1cacf743e4f1122f",
+    "url": "/clima-react/static/js/runtime-main.4fa2276a.js"
+  }
+]);
